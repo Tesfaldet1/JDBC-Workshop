972d057e397c43b8c17d02933eefa42f784cabb7
Tesfaldet Weldemicheal <tweldemicheal@gmail.com>
2022-12-20 17:18:05 +0100

    same change

--- src/main/java/se/lexicon/JDBCConnectionExamples.java
+++ src/main/java/se/lexicon/JDBCConnectionExamples.java
@@ -1,13 +1,10 @@
 package se.lexicon;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JDBCConnectionExamples {
         public static void main(String[] args) {
-            ex1();
+            ex2();
         }
 
         public static void ex1() {
@@ -30,5 +27,36 @@ public class JDBCConnectionExamples {
             }
 
         }
+    public static void ex2() {
+
+        String query = "select * from city where id = ? and Name like ? and CountryCode like ?";
+
+        int cityId = 3;
+        try (
+                Connection connection = MySqlConnection.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        ) {
+
+            preparedStatement.setInt(1, cityId);
+            preparedStatement.setString(2, "H" + "%");
+            preparedStatement.setString(3,"A" + "%");
+
+            try (ResultSet resultSet = preparedStatement.executeQuery();) {
+
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getInt("id"));
+                    System.out.println(resultSet.getString("Name"));
+                    System.out.println(resultSet.getString("CountryCode"));
+                    System.out.println("-------------");
+                }
+
+            }
+
+        } catch (JDBCConnectionException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
     }
 
